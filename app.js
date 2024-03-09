1edb1839217370be22c506b79af2b07729d3bf92
unknown <vovazb1232@gmail.com>
2024-03-10 00:41:01 +0200

    deploy

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const app = require('express')();
 const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || 'localhost';
 
 app.get('/', (req, res) => {
   console.log(123)
   res.status(200).send('Ok')
 })
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`)
+  console.log(`server is running on ${BASE_URL}:${PORT}`)
 })
