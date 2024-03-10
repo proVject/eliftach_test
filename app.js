7c7d562dee1c1d54720ad0a1457416a48c282364
unknown <vovazb1232@gmail.com>
2024-03-10 12:09:48 +0200

    deploy

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const app = require('express')();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4040;
 const BASE_URL = process.env.BASE_URL || 'localhost';
 
 app.get('/', (req, res) => {
-  console.log(123)
+  console.log(process.env)
+  console.log('SSH_PRIVATE_KEY', process.env.SSH_PRIVATE_KEY)
+  console.log('SERVER_IP', process.env.SERVER_IP)
   res.status(200).send('OK')
 })
 app.listen(PORT, () => {
